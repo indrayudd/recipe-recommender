a8e13d28f46ee41a05001e1c3cbee1d56dd3361e
Indrayudd Roy Chowdhury <indro@dhcp-10-174-186-55.umd.edu>
2025-02-19 22:07:30 -0500

    added middleware cuz why not

--- api/debuj.js
+++ api/debuj.js
@@ -1,8 +1,9 @@
+// api/debug.js
 export default function handler(req, res) {
-    // Log them for debugging in Vercel's function logs:
-    console.log("DEBUG HEADERS:", req.headers);
-  
-    // Return them in the response so you can see them in the browser:
-    res.json({ headers: req.headers });
+    res.json({
+      message: 'Hello from /api/debug!',
+      headers: req.headers,
+      userAgent: req.headers['user-agent'],
+    });
   }
   
